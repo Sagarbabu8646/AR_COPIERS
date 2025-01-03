1635e553d18e65f12f49dd6e6644f2235de7c36f
Sagarbabu8646 <139497625+Sagarbabu8646@users.noreply.github.com>
2025-01-03 22:54:56 +0530

    Add files via upload

---
+++ AR .js
@@ -0,0 +1,8 @@
+function product()
+{
+    window.open("C:/Users/mundr/OneDrive/Desktop/AR copiers/AR 2.html")
+}
+function getquotation()
+{
+    window.open("C:/Users/mundr/OneDrive/Desktop/AR copiers/AR 1.html")
+}
